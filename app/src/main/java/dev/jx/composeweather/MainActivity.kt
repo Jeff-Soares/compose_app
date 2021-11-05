09c98d008ef67a0a0250b554135e3c36641a1791
Jeff <jeffersonvsoares@hotmail.com>
2021-11-05 16:37:03 -0300

    Add Search bar with DropDownMenu predictions

--- app/src/main/java/dev/jx/composeweather/MainActivity.kt
+++ app/src/main/java/dev/jx/composeweather/MainActivity.kt
@@ -11,17 +11,24 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import dev.jx.composeweather.model.WeatherDaily
-import dev.jx.composeweather.model.WeatherHourly
+import androidx.compose.ui.unit.toSize
+import dev.jx.composeweather.data.remote.model.googleplaces.PlaceAutocompletePrediction
+import dev.jx.composeweather.data.remote.model.openweather.WeatherDaily
+import dev.jx.composeweather.data.remote.model.openweather.WeatherHourly
+import dev.jx.composeweather.ui.theme.BlueLight
 import dev.jx.composeweather.ui.theme.ComposeWeatherTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,6 +53,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SearchBar() {
     var query by remember { mutableStateOf("") }
+    var predictions by remember { mutableStateOf(listOf<PlaceAutocompletePrediction>()) }
+
+    var showMenu by remember { mutableStateOf(false) }
+    var textFieldSize by remember { mutableStateOf(Size.Zero) }
 
     Surface(
         elevation = 8.dp,
@@ -53,19 +64,44 @@ fun SearchBar() {
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
+            .onGloballyPositioned { coordinates ->
+                // This value is used to assign to the DropDown the same width
+                textFieldSize = coordinates.size.toSize()
+            }
     ) {
         TextField(
             value = query,
             label = { Text(text = "City") },
             maxLines = 1,
-            onValueChange = { query = it },
+            onValueChange = {
+                query = it
+                // Implement google places auto complete service
+            },
             leadingIcon = { Icon(imageVector = Icons.Filled.Search, contentDescription = null) },
             colors = TextFieldDefaults
                 .textFieldColors(
-                    textColor = Color.Black,
+                    textColor = MaterialTheme.colors.secondary,
                     backgroundColor = MaterialTheme.colors.surface
                 )
         )
+        DropdownMenu(
+            expanded = showMenu,
+            onDismissRequest = { showMenu = false },
+            modifier = Modifier
+                .width(with(LocalDensity.current) { textFieldSize.width.toDp() })
+        ) {
+            QueryAutoComplete(predictions = predictions)
+        }
+    }
+}
+
+@Composable
+fun QueryAutoComplete(predictions: List<PlaceAutocompletePrediction>) {
+    predictions.forEach { prediction ->
+        DropdownMenuItem(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Filled.LocationOn, contentDescription = null)
+            Text(text = prediction.description)
+        }
     }
 }
 
@@ -103,14 +139,16 @@ fun WeatherCard() {
                 Image(
                     painter = painterResource(id = R.drawable.ic_precipitation),
                     contentDescription = "Precipitation icon",
-                    modifier = Modifier.size(16.dp)
+                    modifier = Modifier.size(16.dp),
+                    colorFilter = ColorFilter.tint(MaterialTheme.colors.secondary)
                 )
                 Text(text = "Precipitation", modifier = Modifier.padding(horizontal = 4.dp))
                 Spacer(modifier = Modifier.weight(1f))
                 Image(
                     painter = painterResource(id = R.drawable.ic_wind),
                     contentDescription = "Winds icon",
-                    modifier = Modifier.size(16.dp)
+                    modifier = Modifier.size(16.dp),
+                    colorFilter = ColorFilter.tint(MaterialTheme.colors.secondary)
                 )
                 Text(text = "5 km/h Winds", modifier = Modifier.padding(horizontal = 4.dp))
             }
@@ -129,7 +167,7 @@ fun WeatherHourlyCard(weathers: List<WeatherHourly>) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(24.dp)
+                .padding(16.dp)
         ) {
             LazyRow {
                 items(items = weathers) { weather ->
@@ -146,7 +184,11 @@ fun WeatherHourlyItem(weather: WeatherHourly) {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.padding(horizontal = 8.dp)
     ) {
-        Image(painter = painterResource(id = R.drawable.ic_condition), contentDescription = null)
+        Image(
+            painter = painterResource(id = R.drawable.ic_condition),
+            contentDescription = null,
+            modifier = Modifier.size(56.dp)
+        )
         Text(text = "20ºC", modifier = Modifier.padding(vertical = 8.dp))
         Text(text = "9 AM")
     }
@@ -185,7 +227,13 @@ fun WeatherDailyItem(weather: WeatherDaily) {
             .padding(vertical = 8.dp)
     ) {
         Text(text = "Today", modifier = Modifier.padding(vertical = 8.dp))
-        Image(painter = painterResource(id = R.drawable.ic_condition), contentDescription = null)
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.ic_condition),
+                contentDescription = null
+            )
+            Text(text = "40%", modifier = Modifier.padding(vertical = 8.dp), color = BlueLight)
+        }
         Text(text = "19ºC", modifier = Modifier.padding(vertical = 8.dp))
         Text(text = "29ºC")
     }
